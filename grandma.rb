1c5395e6fddf9ed35380ad881f9aa25c30a0a8c6
assane savadogo <assane.savadogo81@gmail.com>
2019-07-25 20:39:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,6 @@
 def speak_to_grandma(phrase)
-  if phrase == 
+  if phrase == "I LOVE YOU GRANDMA!"
+    
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
